ffc5f7817dd8a79288ce4b958c23d24917cc90e8
Morgonz <andresmorgonz@hotmail.com>
2017-05-17 17:16:07 +0200

    Walker delta generator for Thijs

---
+++ Constellation/WalkerDeltaGenerator.m
@@ -0,0 +1,22 @@
+%% Walker Delta Orbit Generator
+% i: t/p/f
+clear all;
+close all;
+clc;
+
+
+alt  = 2000e3; % [m]
+Nsat = 20; % [-], t
+Norb = 5;  % [-], p
+incl = 50; % [deg], i
+
+nsat = Nsat/Norb % [-], s
+omeg = 360/Norb  % [deg], ?
+
+
+
+
+
+
+
+
