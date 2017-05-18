2bff4b17758b65776a1df364ad9a35e5730c0831
thijsvdgaag <thijsvandergaag@gmail.com>
2017-05-18 10:06:31 +0200

    finalised keplerplot & walkerdeltagenerator

--- Constellation/SatelliteConfiguration.m
+++ Constellation/SatelliteConfiguration.m
@@ -4,35 +4,35 @@
 h = 400e3; %Network satellite height [m]
 i = deg2rad(5); %Elevation angle
 
-S_FOV = deg2rad(100/2); %Satellite field of view
+S_FOV = deg2rad(120/2); %Satellite field of view
 R_FOV = deg2rad(120/2); %Rover field of view
 
 %Moon parameters
 %M_moon = 7.342e22; % [kg]
 %G = 6.67408e-11;
-R_M = 1737.1e3; % [m]
+Rm = 1737.1e3; % [m]
 
-R_S = R_M + h;
+Rs = Rm + h;
 
 
 
 %Ground swath width
-a = asin(R_M*cos(i)/(R_S)); %half angle of satellite vision on the Moon
+a = asin(Rm*cos(i)/(Rs)); %half angle of satellite vision on the Moon
 %b = acos(R_M*cos(i)/(R_S))-i; %half angle 
 
 if a>S_FOV
     %Calculate moon coverage angle with antenna FOV
     %b=deg2rad(90)-acos((R_S*sin(deg2rad(S_FOV)))/R_M);
-    b = deg2rad(90) - S_FOV - acos((R_S*sin(S_FOV))/R_M);
-    disp('bug')
+    b = deg2rad(90) - S_FOV - acos((Rs*sin(S_FOV))/Rm);
+
 else
     %Calculate moon coverage angle with Moon FOV + elevation angle (moon surface roughness)
-    b = acos(R_M*cos(i)/(R_S))-i;
+    b = acos(Rm*cos(i)/(Rs))-i;
 end
 
 %Ascending node orbit
 dn = b; %Ascending node spacing
-n = ceil(pi/dn); %Number of orbit planes
+n = ceil(2*pi/dn); %Number of orbit planes
 %phasing
 dp = b; %Phase spacing
 p = ceil(2*pi/dp); %Number of sats per plane
--- Constellation/WalkerDeltaGenerator.m
+++ Constellation/WalkerDeltaGenerator.m
@@ -1,36 +1,50 @@
-function [ const ] = walkerdeltagenerator( i,n,p,h )
+function [ loc_sat ] = walkerdeltagenerator( i,n,p,h )
 %walkerdeltagenerator: generate walker delta orbit parameters based on
 %input requirements
-const = [];
-R_M = 1737.1e3;
 
+% i = 50;
+% n = 6;
+% p = 12;
+% h = 400e3;
+
+Rm = 1737.1e3;
+const = [];
+
+loc_sat = [];
+figure;
 % n = amount of orbit planes
 % p = amount of sats per orbit
-deltaRAAN = pi/n;
+deltaRAAN = 2*pi/n;
 deltaphase = 2*pi/p;
-e = 1;
+e = 0;
 omega = 0;
-ID = 0;
+i = deg2rad(i);
+hold on;
 for idx=1:n %per plane
     ID = idx*100;
-   RAAN = deltaRAAN*(n-1);
+   RAAN = deltaRAAN*(idx-1);
     for jdx=1:p %per sat
         ID = ID + 1;
-        phase = deltaphase*(p-1);
+        phase = deltaphase*(jdx-1);
         
-        append = [ID e i RAAN omega phase h]';
-        const = [const append];
-        keplerplot(append(2),append(3),append(4),append(5),append(6),append(7));
+%         sat = [ID e i RAAN omega phase]';
+%         const = [const sat];
+        [X,Y,Z] = keplerplot(e,i,RAAN,omega,phase,h);
+        size([X,Y,Z])
+        scatter3(X(1),Y(1),Z(1),20,'filled');
+        loc_sat = [loc_sat;X(1) Y(1) Z(1) ID];
     end
+    plot3(X,Y,Z,'LineWidth',1.5);
 end
+%plot moon sphere
 [p,q,r] = sphere(40);
-p = p.*R_M; q = q.*R_M; r = r.*R_M;
+p = p.*Rm; q = q.*Rm; r = r.*Rm;
 
 surf(p,q,r);hold on;
 axis vis3d
 axis equal
 hold off;
-        
+
 
 end
 
