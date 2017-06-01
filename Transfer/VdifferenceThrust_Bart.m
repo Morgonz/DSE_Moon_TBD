9a00cfc43020736f9faef9404925818bb67d34c2
Bart van de Krol <bartvandekrol@gmail.com>
2017-06-01 09:04:53 +0200

    PilsbaasLaurence

--- Transfer/VdifferenceThrust_Bart.m
+++ Transfer/VdifferenceThrust_Bart.m
@@ -9,8 +9,9 @@ VSpiralOut = sqrt(SpiralOut(:,6).^2 + SpiralOut(:,7).^2);
 Vdifference = VReverseTime-VSpiralOut;
 time = ReverseTime(:,2)+SpiralOut(:,2);
 time = time/(60*60*24);
-plot(Thrust,abs(Vdifference))
-%plot(Thrust,time)
-xlabel('Thrust [N]')
-ylabel('V_{difference} [m/s]')
-title('Difference in velocity at L1 between phase 1 and phase 2')
+% plot(Thrust,abs(Vdifference))
+% %plot(Thrust,time)
+% xlabel('Thrust [N]')
+% ylabel('V_{difference} [m/s]')
+% title('Difference in velocity at L1 between phase 1 and phase 2')
+
