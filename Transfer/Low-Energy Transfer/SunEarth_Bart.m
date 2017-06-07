8bded548b01808d547834afba5542cd9da272e8b
Bart van de Krol <bartvandekrol@gmail.com>
2017-06-07 13:22:41 +0200

    Event function added
    An event function is added to SunEarth_Bart which halts the simulation
    if the halo orbit crosses the x-axis after 1 revolution

--- Transfer/Low-Energy Transfer/SunEarth_Bart.m
+++ Transfer/Low-Energy Transfer/SunEarth_Bart.m
@@ -20,10 +20,12 @@ rS = R_Earth + h; %m
 rL = rE*(Mu_Earth/(3*Mu_Sun))^(1/3);
 
 %options of the integrator
-options = odeset('RelTol', 1e-18);
+options = odeset('RelTol', 1e-18,'Events',@Cross);
 T = 10000000; %s
 
-[t,y] = ode113(@SunEarthAcc, [0 2*T], [rE 0 0 0 vE 0 rE+rL-120000000 0 0 1.0675 vE+426 0],options);
+[t,y] = ode113(@SunEarthAcc, [0 7*T], [rE 0 0 0 vE 0 rE+rL-120000000 0 0 1.06749 vE+426 1.260005700065],options);
+%[t,y] = ode113(@SunEarthAcc, [0 -5*T], [rE 0 0 0 vE 0 rE+rL-120000000 0 0 1.06749 vE+426 0],options);
+%[t,y] = ode113(@SunEarthAcc, [0 -5*T], [rE 0 0 0 vE 0 rE+rL-120000000 0 0 1.06749 vE+426 0],options);
 
 yrot = RotatingFrameSunEarth(y);
 
@@ -31,9 +33,34 @@ figure
 hold on
 plot3(yrot(:,7),yrot(:,8),yrot(:,9))
 plot3(rE+rL,0,0,'c*')
+plot3(rE,0,0,'bo')
 hold off
 xlabel('x [m]')
 ylabel('y [m]')
 zlabel('z [m]')
 axis equal
 axis vis3d
+
+function [value,isterminal,direction] = Cross(t,y)
+if y(1)>0
+    tet = atan(y(2)/y(1));
+elseif y(1)<0
+    tet = atan(y(2)/y(1))+pi;
+else
+    if y(2)>0
+        tet = pi/2;
+    else
+        tet = -pi/2;   
+    end
+end
+xEr1 = y(1)*cos(-tet)-y(2)*sin(-tet);
+yEr1 = y(1)*sin(-tet)+y(2)*cos(-tet);
+xSr1 = y(7)*cos(-tet)-y(8)*sin(-tet);
+ySr1 = y(7)*sin(-tet)+y(8)*cos(-tet);
+
+value = ySr1;
+isterminal=1;
+direction=1;
+end
+
+
